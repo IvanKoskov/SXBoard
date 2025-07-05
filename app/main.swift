ab1e8fb38046aad5435e15967a1ec0058d22acff
IvanKoskov <evanmatthewofficial@gmail.com>
2025-07-05 22:33:12 +0700

    Art

--- app/main.swift
+++ app/main.swift
@@ -1,6 +1,14 @@
 import Cocoa
 import Foundation
-
+/*
+   _____  _  __    ____                               __
+  / ___/ | |/ /   / __ )  ____   ____ _   _____  ____/ /
+  \__ \  |   /   / __  | / __ \ / __ `/  / ___/ / __  /
+ ___/ / /   |   / /_/ / / /_/ // /_/ /  / /    / /_/ /
+/____/ /_/|_|  /_____/  \____/ \__,_/  /_/     \__,_/
+ By Ivan Koskov (Evan Matthew)
+ SXMac © 2025 by Ivan Koskov (aka Evan Matthew) is licensed under BSD 3-Clause License
+*/
 
 let Application = NSApplication.shared
 let delegate = AppDelegate()
