09a1d5086c1c9146dc9d922574580593581d3c5a
IvanKoskov <evanmatthewofficial@gmail.com>
2025-07-07 19:11:23 +0700

    Minor changes

---
+++ ClipBoard/dataModel/clipBoardItem.swift
@@ -0,0 +1,8 @@
+//
+//  clipBoardItem.swift
+//  SXBoard
+//
+//  Created by Evan Matthew on 7/7/25.
+//
+
+import Foundation
---
+++ HelperModules/UI/statusBarClip.swift
@@ -0,0 +1,8 @@
+//
+//  statusBarClip.swift
+//  SXBoard
+//
+//  Created by Evan Matthew on 6/7/25.
+//
+
+import Foundation
